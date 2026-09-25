b8458322ee371cc18a0c23820935a36ad0d374be
Shinynecrid <shinynecrid69@gmail.com>
2026-09-25 06:31:30 -0500

    Add DLC ServerBranch Switching

---
+++ arma3managecdlc.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Usage: arma3managecdlc.sh <depotid> <codename> <rootdir> <basedir>
+# Downloads a single Arma 3 Creator DLC via its dedicated Steam depot (on the
+# 'creatordlc' branch) instead of installing the full ~100GB creatordlc branch.
+set -e
+
+APPID=233780
+BRANCH=creatordlc
+DEPOTID="$1"
+CODENAME="$2"
+ROOTDIR="$3"
+BASEDIR="$4"
+
+STEAMCMD="${ROOTDIR}steamcmd.sh"
+APPINFO="${ROOTDIR}appinfo_${APPID}.txt"
+DOWNLOADDIR="${ROOTDIR}steamapps/content/app_${APPID}/depot_${DEPOTID}"
+
+"$STEAMCMD" +login anonymous +app_info_print $APPID +quit > "$APPINFO" 2>&1
+
+GID=$(awk -v depot="\"$DEPOTID\"" -v branch="\"$BRANCH\"" '
+BEGIN { indepot=0; depth=0; inbranch=0 }
+{
+  if (indepot == 0 && $0 ~ ("^[[:space:]]*" depot "[[:space:]]*$")) {
+    indepot=1; depth=0; next
+  }
+  if (indepot == 1) {
+    if ($0 ~ /{/) depth++
+    if ($0 ~ /}/) {
+      depth--
+      if (depth <= 0) { indepot=0; inbranch=0 }
+    }
+    if (inbranch == 0 && $0 ~ ("^[[:space:]]*" branch "[[:space:]]*$")) {
+      inbranch=1; next
+    }
+    if (inbranch == 1 && $0 ~ /"gid"/) {
+      line=$0
+      gsub(/[^0-9]/, "", line)
+      print line
+      exit
+    }
+  }
+}
+' "$APPINFO")
+
+if [ -z "$GID" ]; then
+    echo "ERROR: Could not resolve manifest ID for depot $DEPOTID on branch $BRANCH"
+    exit 1
+fi
+
+echo "Resolved depot $DEPOTID ($CODENAME) to manifest $GID on branch $BRANCH"
+
+"$STEAMCMD" +login anonymous +download_depot $APPID $DEPOTID $GID +quit
+
+if [ ! -d "$DOWNLOADDIR" ]; then
+    echo "ERROR: Depot download did not produce expected folder: $DOWNLOADDIR"
+    exit 1
+fi
+
+cp -rf "$DOWNLOADDIR"/* "$BASEDIR"
+rm -rf "$DOWNLOADDIR"
+
+echo "Installed Creator DLC '$CODENAME' (depot $DEPOTID) into $BASEDIR"
